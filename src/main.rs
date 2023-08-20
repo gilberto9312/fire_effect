4ce31eb0808bb93c70a153bd2b6776465f3d3f3f
Gilberto Asuaje <asuajegilberto@gmail.com>
2023-08-20 15:33:46 -0500

    feat: set texture

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,15 @@ use raylib_ffi::{
     WindowShouldClose,
     BeginDrawing,
     EndDrawing,
-    DrawPixel,
-    Color
+    Color,
+    Image, 
+    enums,
+    LoadTextureFromImage,
+    Texture2D,
+    DrawTexture
 };
+use raylib_ffi::colors::WHITE;
+use std::ffi::c_void;
 
 fn init_gui() {
     let width: i32 = 400;
@@ -39,19 +45,38 @@ fn close_window() {
     }
 }
 
-fn draw_pixel(x: i32, y: i32, color: Color) {
+
+fn load_texture_from_image(img: Image) -> Texture2D {
     unsafe {
-        DrawPixel(x, y, color);
+        LoadTextureFromImage(img)
     }
 }
 
+fn draw_texture(texture: Texture2D, pos_x: i32, pos_y: i32, tint: Color) {
+    unsafe {
+        DrawTexture(texture, pos_x, pos_y, tint)
+    }
+}
 
 fn main() {
    
     init_gui();
+
+     let mut screen_buffer_data= [ Color{r:0, g:0, b:0, a: 0xFF}; 400*300 ];
+
+    let screen_buffer = Image{
+        data: screen_buffer_data.as_mut_ptr() as *mut c_void,
+        width: 400,
+        height: 300,
+        format: enums::PixelFormat::R8g8b8a8 as i32,
+        mipmaps: 1
+    };
+
+     let screen_buffer_texture = load_texture_from_image(screen_buffer);
+
     while ! window_shold_close() {
         begin_drawing();
-        draw_pixel(200, 200, Color{r: 0xFF, g: 0, b: 0, a: 0xFF});
+        draw_texture(screen_buffer_texture, 0,0, WHITE);
         end_drawing()
     }
     println!("hello world");
